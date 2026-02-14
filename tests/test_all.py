b0c79965bdb6c5186154ee886b1e99483d61faf4
Eugen <trushin00332211@gmail.com>
2026-02-15 00:53:59 +0500

    Пробные тесты для модуля masks

--- tests/test_all.py
+++ tests/test_all.py
@@ -20,3 +20,35 @@ def sample_transactions():
 def empty_transactions():
     """Фикстура с пустым списком."""
     return []
+
+"""Тесты для модуля masks."""
+
+@pytest.mark.parametrize("input_number, expected", [
+        ("7000792289606361", "7000 79** **** 6361"),
+        (1234567890123456, "1234 56** **** 3456"),
+    ])
+def test_mask_card_number(input_number, expected):
+        """Тест маски карт (16 цифр)."""
+        assert mask_account_card(input_number) == expected
+
+@pytest.mark.parametrize("invalid_input", [
+    "65784", "6878127452", "", "81645217446586541386494644654"
+])
+def test_mask_card_number_exception(invalid_input):
+    """Проверка на ValueError с длиной карт."""
+    with pytest.raises(ValueError):
+        mask_account_card(invalid_input)
+
+@pytest.mark.parametrize("input_acc, expected", [
+        ("73654108430135874305", "** 4305"),
+        (10000000000000000000, "** 0000"),
+    ])
+def test_mask_account_card_number(input_acc, expected):
+    """Тест маски корректных счетов(20 цифр)."""
+    assert mask_account_card(input_acc) == expected
+
+@pytest.mark.parametrize("invalid_input", ["34524", ""])
+def test_mask_account_card_number_exception(invalid_input):
+    """Проверка на ValueError с длиной счёта."""
+    with pytest.raises(ValueError):
+        mask_account_card(invalid_input)
