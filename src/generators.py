e52ca63b92fb2f7da5f81e5800c80b6182b4574a
Eugen <trushin00332211@gmail.com>
2026-02-21 16:39:29 +0500

    Реализация генератора card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -87,3 +87,15 @@ def filter_by_currency(transactions, currency_code):
 def transaction_descriptions(transactions):
     for item in transactions:
         yield item['description']
+
+def card_number_generator(start, end):
+    for num in range(start, end + 1):
+        num_str = str(num)
+        zeros_need = 16 - len(num_str)
+        card_num = "0" * zeros_need + num_str
+        first = card_num[0:4]
+        second = card_num[4:8]
+        third = card_num[8:12]
+        fourth = card_num[12:16]
+        full_card_num =' '.join([first, second, third, fourth])
+        yield full_card_num
