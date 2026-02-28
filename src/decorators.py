bc4c65acb5be8709e6d04335518c18c2d33bbe6c
Eugen <trushin00332211@gmail.com>
2026-02-28 20:53:53 +0500

    Реализация decorators.py

---
+++ src/decorators.py
@@ -0,0 +1,26 @@
+from functools import wraps
+
+def log(filename):
+    """Декоратор"""
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                result = func(*args, **kwargs)
+                message = f"Функция {func.__name__} выполнилась успешно. Результат: {result}"
+                if filename:
+                    with open(filename, 'a') as f:
+                        f.write(message + '\n')
+                else:
+                    print(message)
+            except Exception as e:
+                message = f"Функция {func.__name__} упала с ошибкой {type(e).__name__}. Аргументы: {args}, {kwargs}"
+                if filename:
+                    with open(filename, 'a') as f:
+                        f.write(message + '\n')
+                else:
+                    print(message)
+                raise
+            return result
+        return wrapper
+    return decorator
