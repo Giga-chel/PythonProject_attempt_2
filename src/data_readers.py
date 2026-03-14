bb9eab32779c83c5087b2402a5a697f2a5050fe9
Eugen <trushin00332211@gmail.com>
2026-03-14 20:34:14 +0500

    Инструментируем в data_readers.py

--- src/data_readers.py
+++ src/data_readers.py
@@ -1,21 +1,24 @@
-import pandas as pd
 import csv
+
+import pandas as pd
+
 
 def financial_transactions_csv(file_path: str) -> list:
     """Функция с CSV-данными о финансовых транзакциях"""
     try:
-        with open(file_path, encoding='utf-8') as csv_file:
-            csv_reader = csv.DictReader(csv_file, delimiter=';')
+        with open(file_path, encoding="utf-8") as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=";")
             data = list(csv_reader)
     except FileNotFoundError:
         data = []
     return data
 
+
 def financial_transactions_xlsx(file_path: str) -> list:
     """Функция с XLSX-данными о финансовых транзакциях"""
     try:
         df = pd.read_excel(file_path)
-        data = df.to_dict('records')
+        data = df.to_dict("records")
     except Exception:
         data = []
     return data
