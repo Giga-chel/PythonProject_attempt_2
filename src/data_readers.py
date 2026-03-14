92f13d8e9909b7c6daa152c4d9741f60105fc060
Eugen <trushin00332211@gmail.com>
2026-03-14 19:41:32 +0500

    Реализация функции financial_transactions_csv

---
+++ src/data_readers.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import csv
+
+def financial_transactions_csv(file_path: str) -> list:
+    """Функция с CSV-данными о финансовых транзакциях"""
+    try:
+        with open(file_path, encoding='utf-8') as csv_file:
+            csv_reader = csv.DictReader(csv_file, delimiter=';')
+            data = list(csv_reader)
+    except FileNotFoundError:
+        data = []
+    return data
+
