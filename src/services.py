d9ddba80009f5ffc5b5a55130b6faa5db0ee38f7
Eugen <trushin00332211@gmail.com>
2026-03-16 20:09:44 +0500

    Реализация функции process_bank_operations

--- src/services.py
+++ src/services.py
@@ -8,3 +8,11 @@ def process_bank_search(data:list[dict], search:str)->list[dict]:
             result.append(item)
     return result
 
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    result = {category: 0 for category in categories}
+    for item in data:
+        for category in categories:
+            if re.search(category.lower(), item['description'], flags=re.IGNORECASE):
+                result[category] += 1
+    return result
